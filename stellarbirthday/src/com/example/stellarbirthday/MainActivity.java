5b06b80cac02455da850ef7fc90d336684872910
rubanovigor <rubanovigor@gmail.com>
2015-02-05 14:44:44 -0300

    remove style.xml (quick solution wo looking in the core of the issue) that cause error after importing project from git

--- stellarbirthday/src/com/example/stellarbirthday/MainActivity.java
+++ stellarbirthday/src/com/example/stellarbirthday/MainActivity.java
@@ -30,25 +30,7 @@ public class MainActivity extends Activity {
 		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-	
 	// -- start google skymap
 	public void onClick_start_googleskymap (View v)
 	{	
